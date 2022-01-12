2b6cc1089de21ab66defa6fdb20c002875131381
carlo defilippis <carlo.defilippis@me.com>
2022-01-12 11:23:06 -0500

    Added drift snippet to head

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const handlePageChange = () => {
+    setCount(count + 1)
+    console.log("Page changed " + count + " times.")
+    window.drift.page();
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Page has changed {count} {count === 1 ? 'time' : 'times'}.
+        </p><button style={{height: '100px', width: '400px', fontSize: 'large'}} onClick={handlePageChange}>Change Page</button>
         <a
           className="App-link"
           href="https://reactjs.org"
